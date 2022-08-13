41d3f58da0c395d063b9fab0633389413ccd3472
Shreya-Karia <shreyakariaz@gmail.com>
2022-08-14 00:13:08 +0530

    Initial commit

---
+++ mysite/eventportal/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django_nextjs.render import render_nextjs_page_sync
+from django.http import HttpResponse
+
+# def index(request):
+#     return HttpResponse("Hello world!")
+
+
+def index(request):
+    return render_nextjs_page_sync(request)
+
+def sign_in(request):
+    return render_nextjs_page_sync(request)
+
+
---
+++ mysite/my-app/pages/sign_in.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function sign_in() {
+  return (
+    <div>sign_in</div>
+  )
+}
+
+export default sign_in
