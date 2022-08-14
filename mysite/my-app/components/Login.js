2c22c3e7921ce43ecf33e0950f22452f6cf50da9
Shreya-Karia <shreyakariaz@gmail.com>
2022-08-14 16:14:40 +0530

    Update/Login Page

---
+++ mysite/my-app/components/Login.js
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div class="container m-auto lg:p-2 md:p-4 p-10">
+      <div className="max-w-sm mx-auto my-24 bg-white px-5 py-10 rounded shadow-xl drop-shadow-xl">
+        <div className="text-center text-[#000428] mb-8">
+          <h1 className="font-bold text-3xl">LOGIN</h1>
+        </div>
+        <form action="#">
+          <div className="mt-5 text-[#004E92]">
+            <label for="username">Email</label>
+            <input
+              type="text"
+              id="username"
+              className="block w-full p-2 border rounded bg-white border-[#000428]"
+            />
+          </div>
+          <div className="mt-5 text-[#004E92]">
+            <label for="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="block w-full p-2 border rounded bg-white border-[#000428]"
+            />
+          </div>
+          <div className="mt-10">
+            <input
+              type="submit"
+              value="Login"
+              className="py-3 bg-gradient-to-r from-[#13547A] to-[#80D0C7] hover:scale-105 hover:bg-transparent rounded text-white text-center w-full"
+            />
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
+export default Login
--- mysite/my-app/pages/sign_in.js
+++ mysite/my-app/pages/sign_in.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import Login from '../components/Login'
+import { motion } from "framer-motion";
 
-function sign_in() {
+const sign_in = () => {
   return (
-    <div>sign_in</div>
+    <div className='relative flex h-screen bg-gradient-to-r from-[#000428] to-[#004E92]'>
+      <motion.div  initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 1 }} className='container m-auto'>
+        <Login />
+      </motion.div>
+    </div>
   )
 }
 
